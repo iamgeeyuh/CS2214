ea867a9986f3f1c045b63f5e2305369255dc99af
Jia Huang <jiahuang2468@gmail.com>
2023-03-28 10:11:47 -0400

    project 2

---
+++ project/project2/sim.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+RAM = [0 for i in range(128)]
+
+def get_instructions():
+    pass
+
+def main():
+    print("hello world")
+
+if __name__ == "__main__":
+    main()
