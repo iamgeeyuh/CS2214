8fda8d7551c9dfeec6093fc3394034a6d2ad9d36
Jia <jiahuang2468@gmail.com>
2023-03-29 12:20:46 -0400

    project 2

--- project/project2/sim.cpp
+++ project/project2/sim.cpp
@@ -38,17 +38,35 @@ int op_jal = 12;
 
 int op_fill = 13;
 
+int op_none = 14;
+
+// arguments
+
+int rgA; // also rgSrc, rgAddr, reg
+int rgB;
+int rgDst;
+int imm;
+
 void load_code(string filename);
+int opcode(uint16_t code);
 
 int main(int argc, char *argv[])
 {
+    bool halt = false;
+    int op;
+    uint16_t code;
     load_code(argv[1]);
-    for (int i = 0; i < 8192; i++)
+    while (!halt)
     {
-        if (ram[i] != 0)
+        
+        code = ram[pc & 0b1111111111111];
+        op = opcode(code);
+        if (op == op_j && (code & 0b1111111111111) == imm)
         {
-            cout << "ram[" << i << "] = " << ram[i] << endl;
+            halt = true;
         }
+        cout << code << endl;
+        pc++;
     }
 }
 
@@ -68,4 +86,56 @@ void load_code(string filename)
             ram[i] = 0;
         }
     }
+}
+
+int opcode(uint16_t code)
+{
+    imm = code & 0b1111111;
+    rgB = (code >> 7) & 0b111;
+    rgA = (code >> 10) & 0b111;
+    switch ((code >> 13) & 0b111)
+    {
+    // opcode in least sig 4 bits
+    case (0):
+        rgDst = (code >> 4) & 0b111;
+        rgB = (code >> 7) & 0b111;
+        rgA = (code >> 10) & 0b111;
+        switch (code & 0b1111)
+        {
+        case (0):
+            return op_add;
+        case (1):
+            return op_sub;
+        case (2):
+            return op_or;
+        case (3):
+            return op_and;
+        case (4):
+            return op_slt;
+        case (8):
+            if (((code >> 4) & 0b111111) == 0)
+            {
+                return op_jr;
+            }
+        }
+    // opcode in most sig 3 bits
+    case (1):
+        return op_addi;
+    case (2):
+        imm = code & 0b1111111111111;
+        return op_j;
+    case (3):
+        imm = code & 0b1111111111111;
+        return op_jal;
+    case (4):
+        return op_lw;
+    case (5):
+        return op_sw;
+    case (6):
+        return op_jeq;
+    case (7):
+        return op_slti;
+    }
+
+    return op_none;
 }
