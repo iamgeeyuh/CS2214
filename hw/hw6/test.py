cec515ff5d3a94c099f6bef691f5a2acf7d47138
Jia Huang <jiahuang2468@gmail.com>
2023-04-14 12:07:59 -0400

    hw7

--- hw/hw6/test.py
+++
@@ -1,8 +0,0 @@
-purchase_price = int(input("Enter the purchase price (in cents): ")) 
-amount_paid = int(input("Enter the amount paid (in cents): ")) 
-change_due = amount_paid - purchase_price 
-quarters = change_due // 25 
-dimes = (change_due % 25) // 10 
-nickels = ((change_due % 25) % 10) // 5 
-pennies = ((change_due % 25) % 10) % 5 
-print("The change due is", quarters, "quarter(s),", dimes, "dime(s),", nickels, "nickel(s), and", pennies, "pennies.")
